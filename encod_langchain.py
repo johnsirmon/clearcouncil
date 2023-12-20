f0b09089a271d85f771a2e5e193bc595624812a7
johnsirmon <johnsirmon@hotmail.com>
2023-12-20 07:02:04 -0500

    updating to use pdfreader

--- encod_langchain.py
+++ encod_langchain.py
@@ -1,49 +1,46 @@
+from asyncio import as_completed
 from dotenv import load_dotenv
 import os
 import re
-from concurrent.futures import ThreadPoolExecutor
-from PyPDF2 import PdfFileReader
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from datetime import datetime
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 # Provide the relative path to the .env file
 load_dotenv()
 
 pdf_dir = 'data/PDFs'
 embeddings = OpenAIEmbeddings()
-#initialize the FAISS vector store
+
+# Initialize the FAISS vector store with actual parameters
 faiss_store = FAISS(
-    embedding_function=None,  # You should provide the actual embedding function here
-    index=None,  # You should provide the actual index object here
-    docstore=None,  # You should provide the actual docstore object here
-    index_to_docstore_id=None  # You should provide the actual index_to_docstore_id function here
+    embedding_function=embeddings,  # Actual embedding function
+    index="IVF1024,Flat",  # Example index type, modify according to your needs
+    docstore=None,  # Example docstore, choose based on your requirement
+    index_to_docstore_id=lambda x: x  # Simplest mapping, can be customized
 )
-# create a splitter to split the text into chunks of 1024 characters (OpenAI's max input length
-# for the API
-splitter = CharacterTextSplitter(max_length=1024)
+
+splitter = CharacterTextSplitter(chunk_size=10000)
 
 def process_pdf(file_path):
     try:
         with open(file_path, 'rb') as f:
-            pdf_reader = PdfFileReader(f)
-            text = ''.join([pdf_reader.getPage(page_num).extractText() for page_num in range(pdf_reader.numPages)])
+            pdf_reader = PdfReader(f)
+            text = ''.join([pdf_reader.pages[page_num].extract_text() for page_num in range(len(pdf_reader.pages))])
         chunks = splitter.split(text)
         for chunk in chunks:
             vector = embeddings.encode(chunk)
-            # Extract metadata from filename
             metadata = extract_metadata(file_path)
-            # Store vector with metadata 
             faiss_store.store(vector, metadata)
     except Exception as e:
         print(f"Error processing {file_path}: {e}")
 
 def extract_metadata(file_path):
-    # Extracting information from the file format
-    # Example: C:\Source\clearcouncil\data\PDFs\2020-09-21 County Council - Full Minutes-1952.pdf
     filename = os.path.basename(file_path)
     pattern = r'(\d{4}-\d{2}-\d{2}) (.*?) - (.*?)-(\d+)\.pdf'
     match = re.match(pattern, filename)
@@ -56,40 +53,23 @@ def extract_metadata(file_path):
         }
     return {}
 
-num_files_to_process = 10  # Set this to the number of files you want to process for testing
-
-# Initialize counter
+num_files_to_process = 10
 processed_files = 0
 
-# Using ThreadPoolExecutor for parallel processing
 with ThreadPoolExecutor(max_workers=10) as executor:
-    # Prepare a list to hold the future tasks
-    futures = []
-    for filename in os.listdir(pdf_dir):
-        if filename.endswith('.pdf') and len(futures) < num_files_to_process:
-            file_path = os.path.join(pdf_dir, filename)
-            futures.append(executor.submit(process_pdf, file_path))
+    futures = [executor.submit(process_pdf, os.path.join(pdf_dir, filename)) 
+               for filename in os.listdir(pdf_dir) 
+               if filename.endswith('.pdf')]
 
-    # Process the futures and update the progress bar
-    for _ in tqdm(as_completed(futures), total=len(futures), desc="Processing PDFs"):
-        pass
+    for future in tqdm(as_completed(futures), total=len(futures), desc="Processing PDFs"):
+        pass  # or handle results if needed
 
-
-# Save the FAISS index to disk
-# Format the current date in YYYYMMDD format
 current_date = datetime.now().strftime("%Y%m%d")
-
-# Construct the filename with descriptive information and the current date
 index_filename = f"council_meetings_faiss_index_{current_date}.index"
-
-# save indexes to a directory data/faiiss_indexes
 save_path = os.path.join('data', 'faiss_indexes')
 full_path = os.path.join(save_path, index_filename)
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-# Save the FAISS index to disk
 faiss_store.save(full_path)
-
-            
