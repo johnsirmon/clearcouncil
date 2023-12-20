66b843ba4f69993ef8fc278409292b65c286cef4
johnsirmon <johnsirmon@hotmail.com>
2023-12-19 19:41:47 -0500

    more changes to pdf encoding, chunking and retrieving

--- scripts/python/encod_langchain.py
+++ scripts/python/encod_langchain.py
@@ -7,8 +7,6 @@ load_dotenv()
 # Set up the OpenAI API key and save it to the environment
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-
-import os
 from PyPDF2 import PdfFileReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
@@ -21,29 +19,26 @@ pdf_dir = 'path/to/pdf_folder'
 embeddings = OpenAIEmbeddings()
 faiss_store = FAISS()
 
-# Iterate over PDF files
+# Iterate over PDF files in the directory
 for filename in os.listdir(pdf_dir):
     if filename.endswith('.pdf'):
         file_path = os.path.join(pdf_dir, filename)
 
-        # Read PDF and extract text
+        # Open the PDF file
         with open(file_path, 'rb') as f:
             pdf_reader = PdfFileReader(f)
             text = ''
+
+            # Extract text from each page
             for page_num in range(pdf_reader.numPages):
                 text += pdf_reader.getPage(page_num).extractText()
 
-        # Split text into chunks
+        # Split text into chunks for efficient encoding
         splitter = CharacterTextSplitter(max_length=1024)  # Adjust max_length as needed
         chunks = splitter.split(text)
 
-        # Encode and store each chunk
+        # Encode each text chunk and store the resulting vector
         for chunk in chunks:
             vector = embeddings.encode(chunk)
-            faiss_store.add(vector)  # May need additional handling for IDs or metadata
-
-
-
-
-
-
+            # Consider adding unique identifiers or metadata for each vector
+            faiss_store.add(vector)
